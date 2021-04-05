a8e6f6a2304cb1c94723421cf9c0ece351e525d2
neangsamoun01 <80339215+neangsamoun01@users.noreply.github.com>
2021-04-05 14:59:22 +0700

    Add files via upload

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,23 +1,14 @@
 import mongoose from 'mongoose';
 
-const userSchema = new mongoose.Schema(
-  {
-    name: { type: String, required: true },
-    email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
-    isAdmin: { type: Boolean, default: false, required: true },
-    isSeller: { type: Boolean, default: false, required: true },
-    seller: {
-      name: String,
-      logo: String,
-      description: String,
-      rating: { type: Number, default: 0, required: true },
-      numReviews: { type: Number, default: 0, required: true },
-    },
+const userSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  email: {
+    type: String, required: true, unique: true, index: true, dropDups: true,
   },
-  {
-    timestamps: true,
-  }
-);
-const User = mongoose.model('User', userSchema);
-export default User;
+  password: { type: String, required: true },
+  isAdmin: { type: Boolean, required: true, default: false },
+});
+
+const userModel = mongoose.model('User', userSchema);
+
+export default userModel;
